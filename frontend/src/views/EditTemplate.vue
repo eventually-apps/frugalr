9327410615f5a7f1848571502e473c22f02ba371
Bejong Yang <bejongyang@gmail.com>
2019-08-04 01:09:22 -0500

    Fixed router. Started styling for Edit Template View.

--- frontend/src/views/EditTemplate.vue
+++ frontend/src/views/EditTemplate.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="edit">
+        <div class="wrapper">
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <h3>Frugalr</h3>
+                </div>
+                <ul class="list-unstyled components">
+                    <p>Dummy Heading</p>               
+                </ul>
+            </nav>
+        </div>
         <div class="content">
-            <h1>Hi</h1>
-        </div>
-        <div class="sidebar">
-        </div>
+            
+        </div>     
     </div>
 </template>
 
@@ -16,6 +24,53 @@ export default class EditTemplate extends Vue {}
 </script>
 
 
-<style lang="sass" scoped>
+<style lang="scss">
+h3 {
+    text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+}
+
+@media (max-width: 768px) {
+    #sidebar {
+        margin-left: -250px;
+    }
+    #sidebar.active {
+        margin-left: 0;
+    }
+}
+
+.wrapper {
+    display: flex;
+    align-items: stretch;
+}
+
+#sidebar {
+    /* don't forget to add all the previously mentioned styles here too */
+    background: #414040;
+    color: #fff;
+    transition: all 0.3s;
+    min-width: 375px;
+    max-width: 375px;
+    min-height: 100vh;
+}
+
+#sidebar .sidebar-header {
+    padding: 20px;
+    background: #383838;
+}
+
+#sidebar ul.components {
+    padding: 20px 0;
+}
+
+#sidebar ul p {
+    color: #fff;
+    padding: 10px;
+}
+
+#sidebar ul li a {
+    padding: 10px;
+    font-size: 1.1em;
+    display: block;
+}
 
 </style>
