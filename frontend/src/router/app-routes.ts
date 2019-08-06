ea979dfee2a12cc6f251397f40495327269a2957
Chad Smith <smith.chad12@gmail.com>
2019-08-05 21:11:25 -0500

    add app routes to store the routes for the app

--- frontend/src/router/app-routes.ts
+++ frontend/src/router/app-routes.ts
@@ -2,19 +2,19 @@ import { IRouter } from './router-entities/IRouter';
 import Home from '../views/Home.vue';
 
 export const appRoutes: IRouter[] = [
+  {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
     {
-        path: '/',
-        name: 'home',
-        component: Home,
-      },
-      {
-        path: '/create',
-        name: 'create',
-        component: () => import('@/views/EditTemplate.vue'),
-      },
-      {
-        path: '/confirmation',
-        name: 'confirmation',
-        component: () => import('@/views/ConfirmationSend.vue'),
-      },
+      path: '/create',
+      name: 'create',
+      component: () => import('@/views/EditTemplate.vue'),
+    },
+    {
+      path: '/confirmation',
+      name: 'confirmation',
+      component: () => import('@/views/ConfirmationSend.vue'),
+    },
 ];
