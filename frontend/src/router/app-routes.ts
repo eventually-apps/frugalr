30ee39fde44ba91b2a4d1873490658c5c035a896
Chad Smith <smith.chad12@gmail.com>
2019-08-05 20:14:36 -0500

    add router entities/types

---
+++ frontend/src/router/app-routes.ts
@@ -0,0 +1,20 @@
+import { IRouter } from './router-entities/IRouter';
+import Home from '../views/Home.vue';
+
+export const appRoutes: IRouter[] = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+      },
+      {
+        path: '/create',
+        name: 'create',
+        component: () => import('@/views/EditTemplate.vue'),
+      },
+      {
+        path: '/confirmation',
+        name: 'confirmation',
+        component: () => import('@/views/ConfirmationSend.vue'),
+      },
+];
